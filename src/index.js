2a2259423e49572025f93fb31d46f8e325aaca85
epyson <epyson@bk.ru>
2024-07-13 18:53:34 +0300

    6

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { SDKProvider } from '@telegram-apps/sdk-react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserNavigator, SDKProvider } from '@telegram-apps/sdk-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <SDKProvider acceptCustomStyles debug>
-    <BrowserRouter>
+    <BrowserNavigator>
       <App />
-    </BrowserRouter>
+    </BrowserNavigator>
   </SDKProvider>,
 );
